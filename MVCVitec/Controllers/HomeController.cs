8c0f7f12f622d67d012fad5a8fffafcfbd2fa221
Jimmy Martin Henriksen <jimm1034@edu.ucl.dk>
2019-10-08 11:53:27 +0200

    Visit logge added

--- MVCVitec/Controllers/HomeController.cs
+++ MVCVitec/Controllers/HomeController.cs
@@ -18,6 +18,8 @@ namespace MVCVitec.Controllers
         }
         public IActionResult Index()
         {
+           string Message = $"Vist  {DateTime.Now.ToLocalTime()}";
+            logger.LogWarning($"Message {Message} ");
             return View();
         }
 
