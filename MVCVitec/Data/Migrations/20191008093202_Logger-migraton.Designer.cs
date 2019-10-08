e196b81196a2f9c391c81c3204204c8efcc89240
skadefar <tobias.j.hansen@gmail.com>
2019-10-08 11:44:45 +0200

    changes

--- MVCVitec/Data/Migrations/20191008093202_Logger-migraton.Designer.cs
+++ MVCVitec/Data/Migrations/20191008093202_Logger-migraton.Designer.cs
@@ -4,14 +4,16 @@ using MVCVitec.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MVCVitec.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191008093202_Logger-migraton")]
+    partial class Loggermigraton
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
