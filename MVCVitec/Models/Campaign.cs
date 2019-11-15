ff77b4907ba8e7e6efcc03dc1f29576b6739fc88
Daniella Clemmensen <dani7135@edu.ucl.dk>
2019-11-15 11:05:13 +0100

    Campaign models

---
+++ MVCVitec/Models/Campaign.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCVitec.Models
+{
+    public class Campaign
+    {
+        public int CampaignId { get; set; }
+        public string CampaignName { get; set; }
+        public string CampaignDescriotion { get; set; }
+        public int CampaignPrice { get; set; }
+        public Product Price {get; set;}
+        public string CampaignRules { get; set; }
+
+
+    }
+}
