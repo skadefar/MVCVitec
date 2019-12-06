77af7df04dba8efdf16b83819c23c0ebb162c9bf
Daniella Clemmensen <dani7135@edu.ucl.dk>
2019-12-06 09:42:53 +0100

    Integrations test class tilføjet

---
+++ intefrations-test/Intergration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace intefrations_test
+{
+    class Intergration
+    {
+        protected readonly HttpClient TestClient;
+        public Intergration()
+        {
+          //  var appFactory = new web
+        }
+    }
+}
---
+++ intefrations-test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("intefrations-test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("intefrations-test")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6d1de5b2-1b9a-40d1-a4b3-fba1c66f6424")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
