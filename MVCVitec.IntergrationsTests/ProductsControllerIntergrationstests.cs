ef2affa1a828b578a2b2882814a6b9af52572fdc
Daniella Clemmensen <dani7135@edu.ucl.dk>
2019-12-12 16:01:26 +0100

    Det virker stadig ikke

---
+++ MVCVitec.IntergrationsTests/ProductsControllerIntergrationstests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MVCVitec.IntergrationsTests
+{
+    public class ProductsControllerIntergrationstests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
--- intefrations-test/Intergration.cs
+++ intefrations-test/Intergration.cs
@@ -1,29 +1,42 @@
 ﻿using System;
+using System.Data.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using ikvm.runtime;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+ 
 
 namespace intefrations_test
 {
     class Intergration
     {
-        protected readonly HttpClient TestClient;
+     /*   protected readonly HttpClient TestClient;
         protected Intergration()
         {
-            var appFactory = new WebApplicationFactory<Startup>();
+            var appFactory = new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(builder =>
+                { builder.ConfigureServices(services =>
+
+                 {
+                     services.RemoveAll(typeof(DataContext));
+                     services.AddDbContext<DbContext>(optionsAction => options 
+                        
+                     });
+                });
             TestClient = appFactory.CreateClient();
         }
 
         protected async Task AuthenticateAsync()
         {
-          //  TestClient.DefaultRequestHeaders.Authorization = AuthenticationHeaderValue.Parse("thing", GetJwtAsyn());
+           // TestClient.DefaultRequestHeaders.Authorization =new AuthenticationHeaderValue("thing", await GetJwtAsyn());
         }
 
-        protected async Task GetJwtAsyn()
+      /*  private async Task<string> GetJwtAsyn()
         {
-            throw new NotImplementedException();
-        }
+        }*/ 
     }
 }
