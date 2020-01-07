d1289b7dc6686a6ccb107feade0c0b290fbf4d9b
Daniella Clemmensen <dani7135@edu.ucl.dk>
2020-01-07 12:51:31 +0100

    // i main øverst

--- MVCVitec/Program.cs
+++ MVCVitec/Program.cs
@@ -11,7 +11,7 @@ namespace MVCVitec
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main(string[] args)//
         {
             //CreateWebHostBuilder(args).Build().Run();
             var host = CreateWebHostBuilder(args).Build();
